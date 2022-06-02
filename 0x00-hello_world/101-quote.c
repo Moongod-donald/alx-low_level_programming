81308f0fa3a6137691d1501e5f7da6f560c6bed8
Donald <donumeh54@gmail.com>
2022-06-02 09:40:29 -0700

    using write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,16 @@
+#include <unistd.h>
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ **/
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, msg, sizeof(msg));
+
+	return (1);
+}
