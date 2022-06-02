2d532cbae3aa57d9cc9eb675626926186d24f499
Donald <donumeh54@gmail.com>
2022-06-02 09:46:46 -0700

    used write() to print a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, msg, sizeof(msg));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
