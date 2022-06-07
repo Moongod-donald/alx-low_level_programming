a5778e8a534de9d25edbc91f3b0f6848f92654f2
Donald <donumeh54@gmail.com>
2022-06-07 12:06:56 -0700

    sums up to int together

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,12 @@
 
 /**
  * add - adds two int together
+ * @c: first int value to be added
+ * @d: second int value to be added
  *
  * Return: result of value added together
  */
-int add (int c, int d)
+int add(int c, int d)
 {
 	return (c + d);
 }
