9d688f26205f94d6f11a55b9d8768e1f7988c2a8
Donald <donumeh54@gmail.com>
2022-06-07 12:04:34 -0700

    sums up to int together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - adds two int together
+ *
+ * Return: result of value added together
+ */
+int add (int c, int d)
+{
+	return (c + d);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar((i * j) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
