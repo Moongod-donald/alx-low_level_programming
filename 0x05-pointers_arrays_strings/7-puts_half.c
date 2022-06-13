85c73b598b92219fd26427d75f7153b2ec846d5b
Donald <donumeh54@gmail.com>
2022-06-13 10:30:41 -0700

    prints half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: accepts a param that's a pointer to a char
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0, len;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	if ((len % 2) != 0)
+	{
+		len = (len - 1) / 2;
+	}
+	else if ((len % 2) == 0)
+	{
+		len = len / 2;
+	}
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+
+}
