10c25194f841c1449b895d7bf01239f7b655bcec
Donald <donumeh54@gmail.com>
2022-06-16 05:15:35 -0700

    compares two strings together

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,22 +11,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0')
+	while (*s1 == *s2)
 	{
-		if (s1[i] < s2[i])
+		if (*s1 == '\0')
 		{
-			return (-15);
+			return (0);
 		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else
-		{
-			i++;
-		}
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
