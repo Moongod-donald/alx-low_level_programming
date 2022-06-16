9f6c5c999d365b5db222011d11d7b77dbfcfc966
Donald <donumeh54@gmail.com>
2022-06-16 04:53:54 -0700

    compares two strings together

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string to another variable
+ * @dest: char pointer that will hold the string
+ * @src: char pointer that holds the string
+ * @n: amount in bytes to be copied
+ *
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
