0ab54b52746cf9a0d89b2f37da14dc719924fdd5
Donald <donumeh54@gmail.com>
2022-06-20 16:31:01 -0700

    search a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string that will be checked
+ * @accept: string that will be compared
+ *
+ * Return: bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}
