68aea285ccbfb95a7397b53bb5f2bec1b35ff4c7
Donald <donumeh54@gmail.com>
2022-06-20 14:48:04 -0700

    write a function that ocates a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string literal to locate in
+ * @c: char to locate
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
