fd5be895d632ba7186ca2d11a0968b13aaf6186c
Donald <donumeh54@gmail.com>
2022-06-20 15:18:01 -0700

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = -0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			return (s + i);
+			return ((s + i) - 1);
 		}
-
 		i++;
 	}
 	return (s = NULL);
