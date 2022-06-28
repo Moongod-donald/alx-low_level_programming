8df6a631cebfd023580ab64500291a604e774a57
Donald <donumeh54@gmail.com>
2022-06-28 13:22:11 -0700

    function that concatenates two strings using a dynamic memory

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: destination
+ * @s2: source
+ *
+ * Return: char.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, len = 0;
+	char *concat;
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (s1[len])
+		len++;
+
+	while (s2[i])
+	{
+		i++;
+	}
+
+	concat = malloc(sizeof(char) * (i + len));
+
+	if (concat == NULL)
+		return (NULL);
+	len = i = 0;
+
+	while (s1[len])
+	{
+		concat[j] = s1[len];
+		len++;
+		j++;
+	}
+
+	while (s2[i])
+	{
+		concat[j] = s2[i];
+		i++;
+		j++;
+	}
+
+	return (concat);
+}
