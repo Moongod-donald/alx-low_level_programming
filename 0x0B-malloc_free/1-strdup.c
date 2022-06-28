76e311e5c16b292a82c0eb872400164904273362
Donald <donumeh54@gmail.com>
2022-06-28 12:17:20 -0700

    function that allocates memory dynamically

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,22 @@ char *_strdup(char *str)
 	int len = 0, i;
 	char *str1;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	while (str[len] != '\0')
 	{
 		len++;
 	}
+	len = len - 1;
 	str1 = malloc(sizeof(char) * len);
-	for (i = 0; i <= len; i++)
+	if (str1 != NULL)
 	{
-		str1[i] = str[i];
+		for (i = 0; i <= len; i++)
+		{
+			str1[i] = str[i];
+		}
 	}
 
 	return (str1);
