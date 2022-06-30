575c6e162500cd04886b470f2c6c63eeeae23c25
Donald <maceka.media@gmail.com>
2022-06-30 15:55:30 -0700

    function that allocates memory and resets the values to 0 like calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,24 +2,45 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocates a memory
- * @nmemb: the bytes to store
- * @size: size data type
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * Return: void.
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
-	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
-
-	array = malloc(nmemb * size);
-	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		array[i] = 0;
-	return (array);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
